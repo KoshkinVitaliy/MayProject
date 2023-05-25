2e8cd0803cb4cb88acdf1479f7d9ba83a8d67b99
VitaliyKoshkin <kosh98vitaliy@outlook.com>
2023-05-25 17:09:46 +0300

    Initial commit

---
+++ src/main/java/org/example/JavaStarter.java
@@ -0,0 +1,78 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class JavaStarter {
+    public JavaStarter() {
+
+    }
+
+    public List<Map<String,Object>> execute(String request) throws Exception {
+        switch (request.substring(0, 6)) {
+            case "INSERT" -> insertValues(request);
+            case "UPDATE" -> updateValues(request);
+            case "DELETE" -> deleteValues(request);
+            case "SELECT" -> selectValues(request);
+            default -> System.out.println("Incorrect request");
+        }
+
+        return new ArrayList<>();
+    }
+
+    private void insertValues(String request) {
+        //"INSERT VALUES ‘lastName’ = ‘Федоров’ ,‘id’=3, ‘age’=40,‘active’=true
+        Map<String, Object> row = new HashMap<>();
+
+
+        request = updateRequest(request);
+        String[] requestArray = request.split(",");
+
+        for (String letter: requestArray) {
+            StringBuilder value = new StringBuilder(letter.length());
+
+            String key = letter.substring(0, letter.indexOf("="));
+            value.append(
+                    letter, letter.indexOf("=")+1, letter.indexOf(
+                            letter.charAt(letter.length()-1))+1);
+
+
+            row.put(key, value);
+
+            System.out.println(row);
+        }
+
+
+    }
+
+    private void updateValues(String request) {
+        System.out.println("UPDATE");
+    }
+
+    private void deleteValues(String request) {
+        System.out.println("DELETE");
+    }
+
+    private void selectValues(String request) {
+        System.out.println("SELECT");
+    }
+
+    private String updateRequest(String request) {
+        String updatedRequest = request.replaceAll(
+                " ",
+                "");
+        updatedRequest = updatedRequest.replace(
+                "INSERTVALUES",
+                "");
+        updatedRequest = updatedRequest.replaceAll(
+                "‘",
+                "");
+        updatedRequest = updatedRequest.replaceAll(
+                "’",
+                "");
+
+        return updatedRequest;
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+       JavaStarter javaStarter = new JavaStarter();
+       javaStarter.execute("INSERT VALUES " +
+               "‘lastName’ = ‘Федоров’ , " +
+               "‘id’=3, ‘age’=40, ‘option’=negative, active=true");
+
+    }
+}
